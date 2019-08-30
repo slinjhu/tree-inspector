bf678307eaa0e77d9c84e24ce4a631213fc90809
Sen Lin <slin@magicleap.com>
2019-08-30 10:51:49 -0700

    Adding resource files to the dist

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(path.join(pwd, 'Readme.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(name='tree-inspector',
-      version='1.0.1',
+      version='1.0.2',
       python_requires='>=3.7.0',
       description='Inspect a Python object with an interactive tree view',
       url='https://github.com/slinjhu/tree-inspector',
@@ -21,5 +21,6 @@ setup(name='tree-inspector',
       ],
       zip_safe=False,
       long_description=long_description,
-      long_description_content_type='text/markdown')
+      long_description_content_type='text/markdown',
+      include_package_data=True)
 
