1ca8a8aaa54e5c09ae4a4e49c437c5ff48beb841
Sen Lin <slin@magicleap.com>
2019-08-30 13:16:07 -0700

    Improved tooltip

--- tests/test_file.py
+++ tests/test_file.py
@@ -2,16 +2,22 @@ from tree_inspector import dump_tree_to_file
 import unittest
 import os
 import uuid
+import numpy
 
 
 class TestFile(unittest.TestCase):
     def test_write(self):
-        obj = {'a': [], 'b': {}}
+        dt = numpy.dtype([('name', numpy.unicode_, 16), ('grades', numpy.float64, (2,))])
+        x = numpy.array([('Sarah', (8.0, 7.0)), ('John', (6.0, 7.0))], dtype=dt)
+        obj = {
+            'a': [],
+            'b': {},
+            'x': x
+        }
         outfile = f'/tmp/{uuid.uuid4()}.html'
 
         dump_tree_to_file(obj, outfile)
         self.assertTrue(os.path.exists(outfile))
-        print(outfile)
 
 
 if __name__ == '__main__':
